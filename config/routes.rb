6b803444e6bce0c9aa066dd77582ea00cd2a2e6a
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2016-04-09 17:44:16 -0600

    finished iter 3 first part, moving on to optional song features

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :artists, only: [:index, :new, :create, :show, :update, :edit, :destroy]
+  # resources :artists, only: [:index, :new, :create, :show, :update, :edit, :destroy]
+  resources :artists do
+    resources :songs, only: [:new, :create]
+  end
+
+  resources :songs, only: [:show]
 
 end
